b70e3b847c18ef6d56c805b019dfd23f09607798
MATHIAS SILVA GALDINO <602727625@alunos.etec.itaqua>
2019-08-05 22:27:04 -0300

    criaçao de pagina de adiçao de pizzas e laço de repetiçao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  catalogo:Array<object> = []
+
+  ionViewDidEnter (){
+     console.log( "EXECUTOU O VIEW DID ENTER")
+     this.catalogo.push({
+       nome:'calabresa maluca gourmet',
+       descricao:'queijo, Catupiry, Batatapalha, cogumelos mangericão e mais uma colher de caviar.',
+       preco:'R$72,00'
+       
+     })
+  }
+ 
 
 }
